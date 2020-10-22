65df68b301a69573e3a38901cd79c8e49ec8e224
FrontTestSite <testsitewinc@gmail.com>
2020-10-22 17:14:54 +0300

    init: task resolved

--- src/index.js
+++ src/index.js
@@ -37,8 +37,40 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+
+    let split = expr.match(/.{1,10}/g);
+    let res1 = '';
+    let res2 = '';
+
+    for (let i = 0; i < split.length; i++) {
+        let arrItem = split[i].split('');
+
+        res1 = '';
+        for( let j = 0; j < arrItem.length; j++) {
+
+            if(arrItem[j] == '1') {
+                if(arrItem[j + 1] == '0') {
+                    res1 += '.';
+                }
+                else if(arrItem[j + 1]  == '1'){
+                    res1 += '-';
+                }
+                j += 1;
+            }
+
+            else if(arrItem[j]  === '*') {
+                res2 += ' ';
+
+                break;
+            }
+
+        }
+        res2 += res1 && MORSE_TABLE[res1];
+    }
+
+    return res2;
 }
 
 module.exports = {
